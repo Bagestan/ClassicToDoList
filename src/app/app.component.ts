852e432d61a961c1b8c0a590dfd2d27c70b22f4f
Andre Bagestan <andrebagestan@gmail.com>
2023-09-20 16:53:15 -0300

    Criado e desenvolvido compontente Header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'ClassicToDoList';
+
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
 
 import { environment } from 'src/environment/environment';
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 import { AngularFireModule } from '@angular/fire/compat';
 import { getDatabase, provideDatabase } from '@angular/fire/database';
 import { getAuth, provideAuth } from '@angular/fire/auth';
-import { NZ_I18N } from 'ng-zorro-antd/i18n';
-import { pt_BR } from 'ng-zorro-antd/i18n';
-import { registerLocaleData } from '@angular/common';
-import pt from '@angular/common/locales/pt';
-import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+
+import { pt_BR } from 'ng-zorro-antd/i18n';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
 import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
 
 registerLocaleData(pt);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HeaderComponent],
   imports: [
     AngularFireModule.initializeApp(environment.firebase),
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
+    AngularFireAuthModule,
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
@@ -34,6 +42,9 @@ registerLocaleData(pt);
     BrowserAnimationsModule,
     AngularFireDatabaseModule,
     NzMessageModule,
+    NzLayoutModule,
+    NzIconModule,
+    NzButtonModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: pt_BR }],
   bootstrap: [AppComponent],
